e0e227384a00496b2c1a62b8fba8ffab8c5c91b9
Goodwin126 <gulindimon@mail.ru>
2022-06-13 15:10:52 +0300

    release v1

---
+++ states/__init__.py
@@ -0,0 +1,6 @@
+from .test import Test
+from .test import Admin
+from .test import Kbgu
+from .test import C_C
+
+
---
+++ states/test.py
@@ -0,0 +1,56 @@
+from aiogram.dispatcher.filters.state import StatesGroup, State
+
+
+class Test(StatesGroup):
+    Q1 = State()
+    Q2 = State()
+    Q3 = State()
+    Q4 = State()
+    Q5 = State()
+    Q6 = State()
+    Q7 = State()
+    Q8 = State()
+    Q9 = State()
+    Q10 = State()
+
+class Admin(StatesGroup):
+    photo = State()
+    name = State()
+    description = State()
+    price = State()
+
+
+class Kbgu(StatesGroup):
+    name = State()
+    protein = State()
+    fat = State()
+    carbohydrate = State()
+    calories = State()
+    check = State()
+
+class C_C(StatesGroup):
+    V1 = State()
+    V2 = State()
+    V3 = State()
+    V4 = State()
+    V5 = State()
+    V6 = State()
+    V7 = State()
+    V8 = State()
+    V9 = State()
+    V10 = State()
+    V11 = State()
+    V12 = State()
+    V13 = State()
+    V14 = State()
+    V15= State()
+    V16 = State()
+    V17 = State()
+    V18 = State()
+    V19 = State()
+    V20 = State()
+    V21 = State()
+    V22 = State()
+    V23 = State()
+    V24 = State()
+
